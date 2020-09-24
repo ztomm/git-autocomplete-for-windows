68dcb042b6f3320c768731174638e8870b1baf48
xkmgt <dev@muratkaraca.de>
2020-09-24 12:47:46 +0200

    list remotes

--- git-autocomplete.lua
+++ git-autocomplete.lua
@@ -11,131 +11,131 @@ local showAliases = {}
 local showBranchAliases = {}
 local switchAliases = {}
 
-function string.starts(str, start)
+function startsWith(str, start)
 	return string.sub(str, 1, string.len(start)) == start
 end
 
-function string.ends(str, _end)
+function endsWith(str, _end)
 	return string.sub(str, string.len(str) - string.len(_end) + 1) == _end
 end
 
 function isAlias(text)
-	if string.starts(text, "git branch") then
+	if startsWith(text, "git branch") then
 		return true
 	end
 
 	for i, _alias in ipairs(branchAliases) do
-		if string.starts(text, "git ".._alias) then
+		if startsWith(text, "git ".._alias) then
 			return true
 		end
 	end
 
-	if string.starts(text, "git checkout") then
+	if startsWith(text, "git checkout") then
 		return true
 	end
 
 	for i, _alias in ipairs(checkoutAliases) do
-		if string.starts(text, "git ".._alias) then
+		if startsWith(text, "git ".._alias) then
 			return true
 		end
 	end
 
-	if string.starts(text, "git diff") then
+	if startsWith(text, "git diff") then
 		return true
 	end
 
 	for i, _alias in ipairs(diffAliases) do
-		if string.starts(text, "git ".._alias) then
+		if startsWith(text, "git ".._alias) then
 			return true
 		end
 	end
 
-	if string.starts(text, "git fetch") then
+	if startsWith(text, "git fetch") then
 		return true
 	end
 
 	for i, _alias in ipairs(fetchAliases) do
-		if string.starts(text, "git ".._alias) then
+		if startsWith(text, "git ".._alias) then
 			return true
 		end
 	end
 
-	if string.starts(text, "git merge") then
+	if startsWith(text, "git merge") then
 		return true
 	end
 
 	for i, _alias in ipairs(mergeAliases) do
-		if string.starts(text, "git ".._alias) then
+		if startsWith(text, "git ".._alias) then
 			return true
 		end
 	end
 
-	if string.starts(text, "git pull") then
+	if startsWith(text, "git pull") then
 		return true
 	end
 
 	for i, _alias in ipairs(pullAliases) do
-		if string.starts(text, "git ".._alias) then
+		if startsWith(text, "git ".._alias) then
 			return true
 		end
 	end
 
-	if string.starts(text, "git push") then
+	if startsWith(text, "git push") then
 		return true
 	end
 
 	for i, _alias in ipairs(pushAliases) do
-		if string.starts(text, "git ".._alias) then
+		if startsWith(text, "git ".._alias) then
 			return true
 		end
 	end
 
-	if string.starts(text, "git rebase") then
+	if startsWith(text, "git rebase") then
 		return true
 	end
 
 	for i, _alias in ipairs(rebaseAliases) do
-		if string.starts(text, "git ".._alias) then
+		if startsWith(text, "git ".._alias) then
 			return true
 		end
 	end
 
-	if string.starts(text, "git reset") then
+	if startsWith(text, "git reset") then
 		return true
 	end
 
 	for i, _alias in ipairs(resetAliases) do
-		if string.starts(text, "git ".._alias) then
+		if startsWith(text, "git ".._alias) then
 			return true
 		end
 	end
 
-	if string.starts(text, "git show") then
+	if startsWith(text, "git show") then
 		return true
 	end
 
 	for i, _alias in ipairs(showAliases) do
-		if string.starts(text, "git ".._alias) then
+		if startsWith(text, "git ".._alias) then
 			return true
 		end
 	end
 
-	if string.starts(text, "git show-branch") then
+	if startsWith(text, "git show-branch") then
 		return true
 	end
 
 	for i, _alias in ipairs(showBranchAliases) do
-		if string.starts(text, "git ".._alias) then
+		if startsWith(text, "git ".._alias) then
 			return true
 		end
 	end
 
-	if string.starts(text, "git switch") then
+	if startsWith(text, "git switch") then
 		return true
 	end
 
 	for i, _alias in ipairs(switchAliases) do
-		if string.starts(text, "git ".._alias) then
+		if startsWith(text, "git ".._alias) then
 			return true
 		end
 	end
@@ -150,17 +150,15 @@ function getBranches()
 
 	local branches = {}
 
-	if string.starts(result, "fatal") == false then
+	if startsWith(result, "fatal") == false then
 		for branch in string.gmatch(result, "[* ]%s.%S+") do
 			if string.sub(branch, 0, 1) == "*" then
 				branch = string.gsub(branch, "* ", "")
+			elseif startsWith(branch, "  remotes/") == true then
+				branch = string.gsub(branch, "  remotes/", "")
 			else
 				branch = string.gsub(branch, "  ", "")
 			end
-			table.insert(branches, branch)
-		end
-		for branch in string.gmatch(result, "  %S+") do
-			branch = string.gsub(branch, "remotes/origin/", "origin ")
 			table.insert(branches, branch)
 		end
 	end
@@ -168,15 +166,40 @@ function getBranches()
 	return branches
 end
 
-function gitAutocompleteBranches(text, first, last)
+function getRemotes()
+	local handle = io.popen("git remote -v 2>&1")
+	local result = handle:read("*a")
+	handle:close()
+
+	local remotes = {}
+
+	if startsWith(result, "fatal") == false then
+		for remote in string.gmatch(result, "%S+") do
+			if string.sub(remote, 0, 1) ~= "(" then
+				table.insert(remotes, remote)
+			end
+		end
+	end
+
+	return remotes
+end
+
+function gitAutocomplete(text)
 	local matchCount = 0
 
 	if isAlias(rl_state.line_buffer) then
 		local branches = getBranches()
-
 		for i, branch in ipairs(branches) do
-			if string.starts(branch, text) then
+			if startsWith(branch, text) then
 				clink.add_match(branch)
+				matchCount = matchCount + 1
+			end
+		end
+
+		local remotes = getRemotes()
+		for i, remote in ipairs(remotes) do
+			if startsWith(remote, text) then
+				clink.add_match(remote)
 				matchCount = matchCount + 1
 			end
 		end
@@ -190,40 +213,40 @@ local result = handle:read("*a")
 handle:close()
 
 for alias in string.gmatch(result, "%.(%S+ %S+)") do
-	if string.ends(alias, " branch") then
+	if endsWith(alias, " branch") then
 		table.insert(branchAliases, string.gmatch(alias, "%S+")())
 	end
-	if string.ends(alias, " checkout") then
+	if endsWith(alias, " checkout") then
 		table.insert(checkoutAliases, string.gmatch(alias, "%S+")())
 	end
-	if string.ends(alias, " diff") then
+	if endsWith(alias, " diff") then
 		table.insert(diffAliases, string.gmatch(alias, "%S+")())
 	end
-	if string.ends(alias, " fetch") then
+	if endsWith(alias, " fetch") then
 		table.insert(fetchAliases, string.gmatch(alias, "%S+")())
 	end
-	if string.ends(alias, " merge") then
+	if endsWith(alias, " merge") then
 		table.insert(mergeAliases, string.gmatch(alias, "%S+")())
 	end
-	if string.ends(alias, " pull") then
+	if endsWith(alias, " pull") then
 		table.insert(pullAliases, string.gmatch(alias, "%S+")())
 	end
-	if string.ends(alias, " push") then
+	if endsWith(alias, " push") then
 		table.insert(pushAliases, string.gmatch(alias, "%S+")())
 	end
-	if string.ends(alias, " rebase") then
+	if endsWith(alias, " rebase") then
 		table.insert(rebaseAliases, string.gmatch(alias, "%S+")())
 	end
-	if string.ends(alias, " reset") then
+	if endsWith(alias, " reset") then
 		table.insert(resetAliases, string.gmatch(alias, "%S+")())
 	end
-	if string.ends(alias, " show") then
+	if endsWith(alias, " show") then
 		table.insert(showAliases, string.gmatch(alias, "%S+")())
 	end
-	if string.ends(alias, " show-branch") then
+	if endsWith(alias, " show-branch") then
 		table.insert(showBranchAliases, string.gmatch(alias, "%S+")())
 	end
-	if string.ends(alias, " switch") then
+	if endsWith(alias, " switch") then
 		table.insert(switchAliases, string.gmatch(alias, "%S+")())
 	end
 end
@@ -235,4 +258,4 @@ for alias in string.gmatch(result, "%.(%S+)") do
 end
 
 clink.arg.register_parser("git", aliases)
-clink.register_match_generator(gitAutocompleteBranches, 1)
+clink.register_match_generator(gitAutocomplete, 1)
