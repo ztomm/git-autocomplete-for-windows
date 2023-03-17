5c08308c4bef53272e0d3d4bf6a8a6d123664a43
Murat Motz <dev@ztom.de>
2023-03-17 16:57:12 +0100

    rewritten skript

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # git-autocomplete-for-windows
 
-Enable autocomplete for git commands and branches on Windows command line (cmd).
+Enable auto-completion for Git commands and branches in the Windows command prompt (cmd).
 
-- Install [Clink](https://chrisant996.github.io/clink/)
-- Copy [git-autocomplete.lua](https://github.com/ztomm/git-autocomplete-for-windows/blob/main/git-autocomplete.lua) into `C:\Users\USERNAME\AppData\local\clink`
-- Restart Windows
+- Install [Clink](https://chrisant996.github.io/clink/) (Bash features for Windows)
+- Copy [git-autocomplete.lua](https://github.com/ztomm/git-autocomplete-for-windows/blob/main/git-autocomplete.lua) into `C:\Users\<username>\AppData\local\clink`
+
+I prefer to deselect `Use enhanced default settings` when installing Clink. This causes suggestions to be displayed as a list on a double-tab.
+
+`CTRL+SPACE` is quite useful with Clink.
+
+Take the feature if you want some color in the CMD. The list comes then also with CTRL+SPACE.
--- git-autocomplete.lua
+++ git-autocomplete.lua
@@ -1,261 +1,78 @@
-local branchAliases = {}
-local checkoutAliases = {}
-local diffAliases = {}
-local fetchAliases = {}
-local mergeAliases = {}
-local pullAliases = {}
-local pushAliases = {}
-local rebaseAliases = {}
-local resetAliases = {}
-local showAliases = {}
-local showBranchAliases = {}
-local switchAliases = {}
+local commandAliases = {
+	"add", "bisect", "branch", "checkout", "clone", "commit", "diff", "fetch", 
+	"grep", "init", "log", "merge", "mv", "pull", "push", "rebase", "reset", 
+	"restore", "rm", "show", "show-branch", "switch", "stash", "status", "tag"
+}
+local branchLocalAliases = {
+	"checkout", "push", "switch"
+}
+local branchRemoteAliases = {
+	"branch", "diff", "fetch", "merge", "pull", "rebase", "reset", "show", "show-branch"
+}
+local resetAliases = {
+	"add", "mv", "restore", "rm"
+}
 
-function startsWith(str, start)
+local gitAutocomplete = clink.generator(10)
+
+local function startsWith(str, start)
 	return string.sub(str, 1, string.len(start)) == start
 end
 
-function endsWith(str, _end)
-	return string.sub(str, string.len(str) - string.len(_end) + 1) == _end
-end
-
-function isAlias(text)
-	if startsWith(text, "git branch") then
-		return true
-	end
-
-	for i, _alias in ipairs(branchAliases) do
-		if startsWith(text, "git ".._alias) then
+local function hasValue(table, value)
+	for k, v in pairs(table) do
+		if v == value then
 			return true
 		end
 	end
-
-	if startsWith(text, "git checkout") then
-		return true
-	end
-
-	for i, _alias in ipairs(checkoutAliases) do
-		if startsWith(text, "git ".._alias) then
-			return true
-		end
-	end
-
-	if startsWith(text, "git diff") then
-		return true
-	end
-
-	for i, _alias in ipairs(diffAliases) do
-		if startsWith(text, "git ".._alias) then
-			return true
-		end
-	end
-
-	if startsWith(text, "git fetch") then
-		return true
-	end
-
-	for i, _alias in ipairs(fetchAliases) do
-		if startsWith(text, "git ".._alias) then
-			return true
-		end
-	end
-
-	if startsWith(text, "git merge") then
-		return true
-	end
-
-	for i, _alias in ipairs(mergeAliases) do
-		if startsWith(text, "git ".._alias) then
-			return true
-		end
-	end
-
-	if startsWith(text, "git pull") then
-		return true
-	end
-
-	for i, _alias in ipairs(pullAliases) do
-		if startsWith(text, "git ".._alias) then
-			return true
-		end
-	end
-
-	if startsWith(text, "git push") then
-		return true
-	end
-
-	for i, _alias in ipairs(pushAliases) do
-		if startsWith(text, "git ".._alias) then
-			return true
-		end
-	end
-
-	if startsWith(text, "git rebase") then
-		return true
-	end
-
-	for i, _alias in ipairs(rebaseAliases) do
-		if startsWith(text, "git ".._alias) then
-			return true
-		end
-	end
-
-	if startsWith(text, "git reset") then
-		return true
-	end
-
-	for i, _alias in ipairs(resetAliases) do
-		if startsWith(text, "git ".._alias) then
-			return true
-		end
-	end
-
-	if startsWith(text, "git show") then
-		return true
-	end
-
-	for i, _alias in ipairs(showAliases) do
-		if startsWith(text, "git ".._alias) then
-			return true
-		end
-	end
-
-	if startsWith(text, "git show-branch") then
-		return true
-	end
-
-	for i, _alias in ipairs(showBranchAliases) do
-		if startsWith(text, "git ".._alias) then
-			return true
-		end
-	end
-
-	if startsWith(text, "git switch") then
-		return true
-	end
-
-	for i, _alias in ipairs(switchAliases) do
-		if startsWith(text, "git ".._alias) then
-			return true
-		end
-	end
-
 	return false
 end
 
-function getBranches()
+local function getBranchesLocal(alias)
 	local handle = io.popen("git branch -a 2>&1")
 	local result = handle:read("*a")
 	handle:close()
-
-	local branches = {}
-
 	if startsWith(result, "fatal") == false then
 		for branch in string.gmatch(result, "[* ]%s.%S+") do
-			if string.sub(branch, 0, 1) == "*" then
-				branch = string.gsub(branch, "* ", "")
-			elseif startsWith(branch, "  remotes/") == true then
-				branch = string.gsub(branch, "  remotes/", "")
-			else
-				branch = string.gsub(branch, "  ", "")
-			end
-			table.insert(branches, branch)
+			branch = string.gsub(branch, "^%*%s*", "")
+      branch = string.gsub(branch, "^%s*remotes/", "")
+      branch = string.gsub(branch, "^%s+", "")
+			clink.argmatcher("git"):addarg(alias):addarg(branch):nofiles()
+			clink.argmatcher("git"):addarg(alias):addarg("origin"):addarg(branch):nofiles()
 		end
 	end
-
-	return branches
 end
 
-function getRemotes()
+local function getBranchesRemote(alias)
 	local handle = io.popen("git remote -v 2>&1")
 	local result = handle:read("*a")
 	handle:close()
-
-	local remotes = {}
-
 	if startsWith(result, "fatal") == false then
-		for remote in string.gmatch(result, "%S+") do
-			if string.sub(remote, 0, 1) ~= "(" then
-				table.insert(remotes, remote)
+		for branch in string.gmatch(result, "%S+") do
+			if string.sub(branch, 0, 1) ~= "(" and startsWith(branch, "http") == false then
+				clink.argmatcher("git"):addarg(alias):addarg(branch):nofiles()
 			end
 		end
 	end
-
-	return remotes
 end
 
-function gitAutocomplete(text)
-	local matchCount = 0
+local function rebuildArgs()
+	clink.argmatcher("git"):reset()
+	clink.argmatcher("git"):addarg(commandAliases)
+end
 
-	if isAlias(rl_state.line_buffer) then
-		local branches = getBranches()
-		for i, branch in ipairs(branches) do
-			if startsWith(branch, text) then
-				clink.add_match(branch)
-				matchCount = matchCount + 1
-			end
-		end
-
-		local remotes = getRemotes()
-		for i, remote in ipairs(remotes) do
-			if startsWith(remote, text) then
-				clink.add_match(remote)
-				matchCount = matchCount + 1
-			end
+function gitAutocomplete:generate(lineState)
+	local alias = lineState:getword(2)
+	if alias then
+		if hasValue(branchLocalAliases, alias) then
+			getBranchesLocal(alias) 
+		elseif hasValue(branchRemoteAliases, alias) then
+			getBranchesLocal(alias)
+			getBranchesRemote(alias) 
+		elseif hasValue(resetAliases, alias) then
+			rebuildArgs()
 		end
 	end
-
-	return matchCount > 0
 end
 
-local handle = io.popen("git config --get-regex alias")
-local result = handle:read("*a")
-handle:close()
-
-for alias in string.gmatch(result, "%.(%S+ %S+)") do
-	if endsWith(alias, " branch") then
-		table.insert(branchAliases, string.gmatch(alias, "%S+")())
-	end
-	if endsWith(alias, " checkout") then
-		table.insert(checkoutAliases, string.gmatch(alias, "%S+")())
-	end
-	if endsWith(alias, " diff") then
-		table.insert(diffAliases, string.gmatch(alias, "%S+")())
-	end
-	if endsWith(alias, " fetch") then
-		table.insert(fetchAliases, string.gmatch(alias, "%S+")())
-	end
-	if endsWith(alias, " merge") then
-		table.insert(mergeAliases, string.gmatch(alias, "%S+")())
-	end
-	if endsWith(alias, " pull") then
-		table.insert(pullAliases, string.gmatch(alias, "%S+")())
-	end
-	if endsWith(alias, " push") then
-		table.insert(pushAliases, string.gmatch(alias, "%S+")())
-	end
-	if endsWith(alias, " rebase") then
-		table.insert(rebaseAliases, string.gmatch(alias, "%S+")())
-	end
-	if endsWith(alias, " reset") then
-		table.insert(resetAliases, string.gmatch(alias, "%S+")())
-	end
-	if endsWith(alias, " show") then
-		table.insert(showAliases, string.gmatch(alias, "%S+")())
-	end
-	if endsWith(alias, " show-branch") then
-		table.insert(showBranchAliases, string.gmatch(alias, "%S+")())
-	end
-	if endsWith(alias, " switch") then
-		table.insert(switchAliases, string.gmatch(alias, "%S+")())
-	end
-end
-
-local aliases = {}
-
-for alias in string.gmatch(result, "%.(%S+)") do
-  table.insert(aliases, alias)
-end
-
-clink.arg.register_parser("git", aliases)
-clink.register_match_generator(gitAutocomplete, 1)
+rebuildArgs()
